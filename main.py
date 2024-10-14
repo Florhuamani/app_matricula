58931fe08a00a2bedb0576391c16ff0581143cfb
Florhuamani <lucanahuamaniflormilagroos@gmail.com>
2024-10-14 10:16:29 -0500

    feat:Tarea Hecha
    Ampliamos los comandos en main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 - Mostrar la lista de todos los matriculados.
 - Filtrar matriculados por programa de estudio.
 """
-Lista_Alumnos=[]
+"""Lista_Alumnos=[]
 
 Nombre=input("Ingrese su nombre:")
 Apellido=input("Ingrese su apellido:")
@@ -25,10 +25,10 @@ Lista_Alumnos.append(Alumno2)
 #fin del problema
 
 #deseo mostrar un menú con las opciones de agregar un nuevo alumno y salir del programa.
-print(Lista_Alumnos)
+print(Lista_Alumnos)"""
 
 
-# Otra manera de resolver .
+"""# Otra manera de resolver .
 Lista_Alumnos=[]
 while True:
     print("Menu:")
@@ -41,11 +41,11 @@ while True:
         # ingresar datos del estudiante 
         nombre = input("Ingrese el nombre del nuevo alumno: ")
         apellido = input("Ingrese el apellido del nuevo alumno: ")
-
-        # creun un diccionario para un nuevo estudiante 
+       
+        # crear un nuevo diccionario para un nuevo estudiante 
         nuevo_alumno = {
             "Nombre": nombre,
-            "Apellido": apellido
+            "Apellido": apellido,
         }
 
         # añadir un nuevo estudiante a la lista 
@@ -56,10 +56,58 @@ while True:
     elif opcion == "2":
         # mostrara la lista de estudiantes 
         for alumno in Lista_Alumnos:
-            print(f"{alumno['Nombre']} {alumno['Apellido']}")
+            print(f"{alumno['Nombre']} {alumno['Apellido']} ")
 
     elif opcion == "3":
         print("Nos vemos luego")
         break
     else:
-        print("Opción incorrecta")
+        print("Opción incorrecta")"""
+
+lista_alumnos=[]
+def mensaje_menu():
+    menu_opciones="""
+        ...........Bienvenido al sistema!...........
+        ................ Registrese ................
+                        elije lo que deseas hacer: 
+        1. Escribe [s] si deseas registrar un alumno
+        2. Escribe [n] si deseas salir del programa
+        Escribe tu respuesta: """
+    return menu_opciones
+
+def ingresar_datos_alumnos():
+    id=len(lista_alumnos)+1
+    cui=int(input("Ingrese el numero de su dni: "))
+    nombre=input("Ingrese el nombre del alumno: ")
+    apellido=input("Ingrese el apellido del alumno: ")
+    numero_celular=int(input("Ingrese su numero de celular: "))
+    programa_estudio=input("Ingrese programa de estudio: ")
+    ciclo_academico=input("Ingrese su ciclo academico: ")
+    email=input("Ingrese su correo electronico: ")
+    guardar_datos_alumnos(id,cui,nombre,apellido,numero_celular,programa_estudio,ciclo_academico,email)
+
+def guardar_datos_alumnos(id,cui,nombre,apellido,numero_celular,programa_estudio,ciclo_academico,email):
+    alumno={
+            "id":id,
+            "cui":cui,
+            "nombre":nombre,
+            "apellido":apellido,
+            "numero_celular":numero_celular,
+            "programa_estudio":programa_estudio,
+            "ciclo_academico":ciclo_academico,
+            "email":email
+            }
+    lista_alumnos.append(alumno)
+
+while True:
+    menu_opciones=input(mensaje_menu())
+
+    if menu_opciones.lower()=="n":
+        print("saliendo del Sistema")
+        break
+    elif menu_opciones.lower()=="s":
+        ingresar_datos_alumnos()  
+    else:
+        print("Opcion errónea")
+
+print(lista_alumnos)
